ccad9d55104ec42ad14730d1295d913e4e67ea10
imanw <imanw77@gmail.com>
2019-10-26 12:49:37 +0330

    Re-design the webpage

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,48 @@
       <!-- Start of Container -->
       <div class="search-area">
         <div class="search-items">
-          <p>What do you looking for?</p>
-          <input type="text" v-model="search_term" v-on:input="search(search_term)"/>
-          <p>{{ data_counter }} results</p>
-          <span>Filtering</span>
-          <select name="scf_select"  v-model="scf_select_value" @change="set_url_by_type(scf_select_value)">
-            <option value="story">Filter by Story</option>
-            <option value="comment">Filter by Comment</option>
-            <option value="front_page">Filter by Front Page</option>
-          </select>
-          <span>Filtering</span>
-          <select name="lr_select" v-model="lr_select_value" @change="set_url_by_lat(lr_select_value)">
-            <option value="last">Latest</option>
-            <option value="revelant">Revelant</option>
-          </select>
-          <span>Filtering</span>
-          <select name="hits_select" v-model="hits_select_value" @change="set_news_hits(hits_select_value)">
-            <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="30">30</option>
-          </select>
+          <div class="search-container">
+            <p>What do you looking for?</p>
+            <input type="text" v-model="search_term" v-on:input="search(search_term)" />
+            <p>{{ data_counter }} results</p>
+          </div>
+          <div class="filter-container">
+            <div class="f-item">
+              <span>Filtering&nbsp;</span>
+              <select
+                name="scf_select"
+                v-model="scf_select_value"
+                @change="set_url_by_type(scf_select_value)"
+              >
+                <option value="story">Filter by Story</option>
+                <option value="comment">Filter by Comment</option>
+                <option value="front_page">Filter by Front Page</option>
+              </select>
+            </div>
+            <div class="f-item">
+              <span>last or revelant&nbsp;</span>
+              <select
+                name="lr_select"
+                v-model="lr_select_value"
+                @change="set_url_by_lat(lr_select_value)"
+              >
+                <option value="last">Latest</option>
+                <option value="revelant">Revelant</option>
+              </select>
+            </div>
+            <div class="f-item">
+              <span>News per page&nbsp;</span>
+              <select
+                name="hits_select"
+                v-model="hits_select_value"
+                @change="set_news_hits(hits_select_value)"
+              >
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+              </select>
+            </div>
+          </div>
         </div>
       </div>
       <div class="news-area">
@@ -43,7 +65,7 @@
         </div>
       </div>
       <div class="pagination">
-        <span v-for="i in pages" :key="i" v-bind="page" @click="get_next_page(i)">{{ i }}</span>
+        <span v-for="i in pages" :key="i"  v-bind="page" @click="get_next_page(i)">{{ i }}</span>
       </div>
       <!-- End of Container -->
     </div>
@@ -59,17 +81,17 @@ export default {
     return {
       news: [],
       url: "http://hn.algolia.com/api/v1/search?",
-      scf_select_value: '',
-      lr_select_value: '',
-      hits_select_value: '',
-      search_term: '',
-      data_counter: '',
+      scf_select_value: "",
+      lr_select_value: "",
+      hits_select_value: "",
+      search_term: "",
+      data_counter: "",
       pages: 0,
-      page: 0
+      page: 0,
+
     };
   },
   methods: {
-
     get_currect_time(timestamp) {
       let date = new Date(timestamp * 1000);
       let year = date.getFullYear();
@@ -78,48 +100,50 @@ export default {
       return year;
     },
     set_url_by_type(type) {
-      if (type === 'story') {
+      if (type === "story") {
         this.url = `http://hn.algolia.com/api/v1/search?tags=story`;
       }
-      if (type === 'comment') {
+      if (type === "comment") {
         this.url = `http://hn.algolia.com/api/v1/search?tags=comment`;
       }
-      if (type === 'front_page') {
+      if (type === "front_page") {
         this.url = `http://hn.algolia.com/api/v1/search?tags=front_page`;
       }
       this.get_data();
     },
     set_url_by_lat(type) {
-      if (type === 'last') {
-        this.url = this.url.replace('search', 'search_by_date') + 'tags=story';
+      if (type === "last") {
+        this.url = this.url.replace("search", "search_by_date") + "tags=story";
       }
-      if (type === 'revelant') {
+      if (type === "revelant") {
         this.url = `http://hn.algolia.com/api/v1/search?`;
       }
       this.get_data();
     },
     set_news_hits(hits) {
-      this.url = this.url + `&hitsPerPage=${hits}`
+      this.url = this.url + `&hitsPerPage=${hits}`;
       this.get_data();
     },
     search(search_term) {
-      this.url = this.url + `&query=${search_term}`
+      this.url = this.url + `&query=${search_term}`;
       this.get_data();
     },
     get_next_page(page) {
       this.url += `&page=${page}`;
-      this.get_data();  
+      this.get_data();
     },
     get_data: function() {
-      this.news = [],
-      axios.get(this.url).then(response => {
-        response.data.hits.forEach(item => {
-          item.created_at = this.get_currect_time(item.created_at_i);
-          this.news.push(item);
+      (this.news = []),
+        axios.get(this.url).then(response => {
+          response.data.hits.forEach(item => {
+            item.created_at = this.get_currect_time(item.created_at_i);
+            this.news.push(item);
+          });
+          this.data_counter = response.data.nbHits
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+          this.pages = response.data.nbPages;
         });
-        this.data_counter = (response.data.nbHits).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        this.pages = response.data.nbPages;
-      });
     }
   },
   mounted() {
